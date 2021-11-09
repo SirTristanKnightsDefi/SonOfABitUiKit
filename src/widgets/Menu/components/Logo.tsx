a34cc2ba04ce1e0df932bc19745985b33be30572
Sir Tristan <sirtris@knightsdefi.com>
2021-11-08 20:29:28 -0500

    Initial commit

---
+++ src/widgets/Menu/components/Logo.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import Flex from "../../../components/Flex/Flex";
+import { HamburgerIcon, HamburgerCloseIcon } from "../icons";
+import MenuButton from "./MenuButton";
+
+interface Props {
+  isPushed: boolean;
+  isDark: boolean;
+  togglePush: () => void;
+  href: string;
+  banner?: string;
+}
+
+const StyledLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  .mobile-icon {
+    width: 32px;
+    ${({ theme }) => theme.mediaQueries.nav} {
+      display: none;
+    }
+  }
+  .desktop-icon {
+    width: 32px; // update with new banner
+    display: none;
+    ${({ theme }) => theme.mediaQueries.nav} {
+      display: block;
+    }
+  }
+`;
+
+const Logo: React.FC<Props> = ({ isPushed, togglePush, isDark, href, banner }) => {
+  const isAbsoluteUrl = href.startsWith("http");
+  if(isDark){
+    banner = 'https://gateway.pinata.cloud/ipfs/QmUD6LcJfoFDSwzDKdewzUmnaVH6q389c1Xo5aqjcTFf3M';
+  } else {
+    banner = 'https://gateway.pinata.cloud/ipfs/QmUD6LcJfoFDSwzDKdewzUmnaVH6q389c1Xo5aqjcTFf3M';
+  }
+
+  return (
+    <Flex>
+      <MenuButton aria-label="Toggle menu" onClick={togglePush} mr="24px">
+        {isPushed ? (
+          <HamburgerCloseIcon width="24px" color="textSubtle" />
+        ) : (
+          <HamburgerIcon width="24px" color="textSubtle" />
+        )}
+      </MenuButton>
+      {isAbsoluteUrl ? (
+        <StyledLink as="a" href={href} aria-label="SOAB">
+          <img
+            src="https://gateway.pinata.cloud/ipfs/QmUD6LcJfoFDSwzDKdewzUmnaVH6q389c1Xo5aqjcTFf3M"
+            alt="SOAB Logo"
+            className="mobile-icon"
+          />
+          <img
+            src={banner}
+            alt="SOAB Logo"
+            className="desktop-icon"
+          />
+        </StyledLink>
+      ) : (
+        <StyledLink to={href} aria-label="SOAB">
+          <img
+            src="hhttps://gateway.pinata.cloud/ipfs/QmUD6LcJfoFDSwzDKdewzUmnaVH6q389c1Xo5aqjcTFf3M"
+            alt="SOAB Logo"
+            className="mobile-icon"
+          />
+          <img
+            src={banner}
+            alt="SOAB Logo"
+            className="desktop-icon"
+          />
+        </StyledLink>
+      )}
+    </Flex>
+  );
+};
+
+export default Logo;
